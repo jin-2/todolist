d1aa38bba90ef58be254973d8ec6e64fccf0559a
sujin2 <sujinlee2@jobkorea.co.kr>
2024-11-13 22:28:50 +0900

    feat: Fix ButtonCheck styling

--- src/components/ui/ButtonCheck/ButtonCheck.tsx
+++ src/components/ui/ButtonCheck/ButtonCheck.tsx
@@ -23,23 +23,39 @@ const ButtonCheck = ({ checked = false, onClick }: ButtonCheckProps) => {
 export default ButtonCheck;
 
 const StyledButtonCheck = styled.button`
-  width: calc(32px + 8px);
-  height: calc(32px + 8px);
-  border-radius: 9999px;
-  border: 1px solid #e5e5e5;
-  animation: background-color 0.3s ease-in-out;
+  position: relative;
+  padding: 4px;
+  width: calc(32px + 4px * 2);
+  height: calc(32px + 4px * 2);
+
+  &::after {
+    content: "";
+    z-index: 1;
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    box-sizing: border-box;
+    display: block;
+    width: calc(100% - 4px * 2);
+    height: calc(100% - 4px * 2);
+    border-radius: 9999px;
+    border: 1px solid #e5e5e5;
+    animation: background-color 0.3s ease-in-out;
+  }
 
   > svg {
+    z-index: 2;
+    position: relative;
     display: none;
     filter: invert(1);
   }
 
-  &.checked {
+  &.checked::after {
     background-color: #2182f3;
     border-color: #2182f3;
+  }
 
-    > svg {
-      display: inline-block;
-    }
+  &.checked > svg {
+    display: inline-block;
   }
 `;
